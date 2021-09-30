720c17e555674f01622b82a1347966307512befb
syahirahshamshudin <sharifahsyahirah1351@gmail.com>
2021-09-30 10:47:19 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan kedua</title>
+</head>
+<body>
+    <h1>Hello World</h1>
+
+    <?php 
+    $name = 'Syahirah';
+    $age = 15 + 5;
+
+    echo '<br>';
+    echo "My name is $name. I am $age years old.";
+    
+    ?>
+
+</body>
+</html>
